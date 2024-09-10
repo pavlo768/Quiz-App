c375abbdea95ed5005ffead97a069753f44a5e68
Pavlo <155533645+pavlo768@users.noreply.github.com>
2024-09-10 16:40:38 +0300

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [  
+    {  
+        question: "Which device is required for the Internet connection?",  
+        option1: "Modem",  
+        option2: "Router",  
+        option3: "LAN Cable",  
+        option4: "Pen Drive",  
+        ans: 1,  
+    },  
+    {  
+        question: "What does HTML stand for?",  
+        option1: "Hyper Text Markup Language",  
+        option2: "High Text Markup Language",  
+        option3: "Hyperlink and Text Markup Language",  
+        option4: "None of the above",  
+        ans: 1,  
+    },  
+    {  
+        question: "Which of the following is a JavaScript framework?",  
+        option1: "React",  
+        option2: "Laravel",  
+        option3: "Django",  
+        option4: "Flask",  
+        ans: 1,  
+    },  
+    {  
+        question: "What is the purpose of CSS?",  
+        option1: "To structure the content",  
+        option2: "To define the layout and design",  
+        option3: "To create server-side logic",  
+        option4: "To manage databases",  
+        ans: 2,  
+    },  
+    {  
+        question: "Which CSS property is used to change the text color of an element?",  
+        option1: "font-color",  
+        option2: "text-color",  
+        option3: "color",  
+        option4: "text-style",  
+        ans: 3,  
+    }  
+];
